63073684b4641a102df2ab50f8d4d3d9e3c309f8
CuB1z <cub1zdev@gmail.com>
2024-05-29 23:45:09 +0200

    style: add active page headerlink styling

--- components/header/HeaderLink.jsx
+++ components/header/HeaderLink.jsx
@@ -1,8 +1,13 @@
+"use client"
 import styles from "@styles/components/HeaderLink.module.css"
+import { usePathname } from "next/navigation"
 
 export default function HeaderLink({ href, children }) {
+    const router = usePathname()
+    const activeStyle = href === router ? styles.active : ""
+    
     return (
-        <a className={styles.a} href={href}>
+        <a className={`${styles.a} ${activeStyle}`} href={href}>
             {children}
         </a>
     )
