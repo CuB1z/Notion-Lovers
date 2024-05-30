ed449f10842c2c50345d9f014159afc95850b9f3
CuB1z <cub1zdev@gmail.com>
2024-05-30 12:03:57 +0200

    moved to GridLayout

--- app/content/[id]/page.jsx
+++ app/content/[id]/page.jsx
@@ -1,8 +1,5 @@
-"use client"
-import { usePathname } from "next/navigation"
-import { useEffect, useState } from "react"
-import Layout from "@/layouts/Layout"
-import { getPages } from "@/utils/notion"
+import GridLayout from "@components/GridLayout"
+import { getChildDatabasePages, getPages, getPageTitle } from "@/utils/notion"
 
 // This function gets called at build time on server-side.
 export async function getStaticPaths() {
@@ -15,32 +12,12 @@ export async function getStaticPaths() {
     return { paths, fallback: false }
 }
 
-export default function Page() {
-    const id = usePathname().split("/").pop()
-    const [data, setData] = useState(null)
-
-    useEffect(() => {
-        fetch(`/api?id=${id}`)
-            .then((response) => response.json())
-            .then((data) => setData(data))
-    }, [id])
-
+export default async function Page({ params }) {
+    const id = params.id
+    const data = await getChildDatabasePages(id)
+    const title = await getPageTitle(id)
 
     return (
-        <Layout>
-            <h1>Title + {id}</h1>
-            <section className="content">
-                {
-                    data ? data.map((page) => {
-                        return (
-                            <div key={page.id}>
-                                <h2>{page.title}</h2>
-                                <p>{page.tag?.name}</p>
-                            </div>
-                        )
-                    }) : "Loading..."
-                }
-            </section>
-        </Layout>
+        <GridLayout data={data} title={title}/>
     )
 }
