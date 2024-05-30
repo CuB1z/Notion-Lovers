7cfb5fd3d807b49d34af030cd96a7a2231726172
CuB1z <cub1zdev@gmail.com>
2024-05-30 21:40:29 +0200

    chore: Refactor page.jsx to remove getStaticPaths function

--- app/content/[id]/[subid]/page.jsx
+++ app/content/[id]/[subid]/page.jsx
@@ -2,33 +2,6 @@ import { getChildDatabasePages, getPages, getPageContent } from "@/utils/notion"
 import { NotionPage } from "@/components/notion/renderer"
 import Layout from "@/layouts/Layout"
 
-// This function gets called at build time on server-side.
-export async function getStaticPaths() {
-    // Fetch all pages
-    const pages = await getPages()
-
-    // For each page, fetch its child pages
-    const childPagesPromises = pages.map(async (page) => {
-        if (!page.id) return []
-        const childPages = await getChildDatabasePages(page.id)
-        return childPages.map((childPage) => ({
-            params: {
-                id: page.id.toString(),
-                subid: childPage.id.toString(),
-            },
-        }))
-    })
-
-    // Wait for all promises to resolve
-    const childPagesArrays = await Promise.all(childPagesPromises)
-
-    // Flatten the array of arrays into a single array
-    const paths = childPagesArrays.flat()
-
-    // Return the paths along with the fallback option
-    return { paths, fallback: true }
-}
-
 export default async function Page({ params }) {
     const id = params.subid
     const content = await getPageContent(id)
--- app/content/[id]/page.jsx
+++ app/content/[id]/page.jsx
@@ -1,17 +1,6 @@
 import GridLayout from "@components/GridLayout"
 import { getChildDatabasePages, getPages, getPageTitle } from "@/utils/notion"
 
-// This function gets called at build time on server-side.
-export async function getStaticPaths() {
-    const pages = await getPages()
-
-    const paths = pages.map((page) => {
-        return { params: { id: page.id } }
-    })
-
-    return { paths, fallback: false }
-}
-
 export default async function Page({ params }) {
     const id = params.id
     const data = await getChildDatabasePages(id)
