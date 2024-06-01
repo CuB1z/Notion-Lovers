e49e9c678ad22edcfa040e31acff2c8de7702c03
CuB1z <cub1zdev@gmail.com>
2024-06-01 10:55:40 +0200

    style: fix styling error

--- app/content/[id]/[subid]/page.jsx
+++ app/content/[id]/[subid]/page.jsx
@@ -1,14 +1,17 @@
-import { getChildDatabasePages, getPages, getPageContent } from "@/utils/notion"
+import styles from "@styles/pages/contentPage.module.css"
+import { getPageContent } from "@/utils/notion"
 import { NotionPage } from "@/components/notion/renderer"
-import Layout from "@/layouts/Layout"
+import ContentLayout from "@/layouts/ContentLayout"
 
 export default async function Page({ params }) {
     const id = params.subid
     const content = await getPageContent(id)
 
     return (
-        <Layout>
-            <NotionPage recordMap={content} rootPageId={id} />
-        </Layout>
+        <ContentLayout>
+            <div className={styles.notion_container}>
+                <NotionPage recordMap={content} rootPageId={id} />
+            </div>
+        </ContentLayout>
     )
 }
