c60dce03161a0799c4a73b485397aac2c38be23b
CuB1z <cub1zdev@gmail.com>
2024-06-08 00:47:53 +0200

    chore: Update SideBarItem component to use new list item icon and add title styling

--- components/sidebar/SideBarItem.jsx
+++ components/sidebar/SideBarItem.jsx
@@ -3,8 +3,8 @@ import styles from "@styles/components/SideBarItem.module.css"
 export default function SideBarItem({ item }) {
     return (
         <a className={styles.item} href={item.url}>
-            <img className={styles.img} src="/assets/arrow-item.svg" alt="arrow" />
-            <span>{item.title}</span>
+            <img className={styles.img} src="/assets/list-item.svg" alt="arrow" />
+            <span className={styles.title}>{item.title}</span>
         </a>
     )
 }
