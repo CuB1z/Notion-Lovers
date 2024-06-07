c4c60cb7f6a709c5c285cbb86a347c3508bd8e50
CuB1z <cub1zdev@gmail.com>
2024-06-07 23:51:33 +0200

    refactor: Refactor GridLayout and notion functions to render data from subdatabases in a better way

---
+++ components/DataSection.jsx
@@ -0,0 +1,35 @@
+import styles from "@styles/components/DataSection.module.css"
+import Card from "@components/Card"
+
+export default function DataSection({ title, content }) {
+    if (content.length === 0) return <></>
+
+    return (
+        <section className={styles.section}>
+            {
+                title &&
+                <>
+                    <div className={styles.title_section}>
+                        <h2>{title}</h2>
+                    </div>
+                </>
+            }
+            <div className={styles.content}>
+                {
+                    content.map((element) => {
+                        return (
+                            <Card
+                                key={element.id}
+                                title={element.title}
+                                description={element.description}
+                                tag={element.tag?.name}
+                                id={element.id}
+                                url={element.url}
+                            />
+                        )
+                    })
+                }
+            </div>
+        </section>
+    )
+}
