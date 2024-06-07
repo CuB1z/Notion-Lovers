9a3d9585d915b60f882b1dd082d8cb341bbb017e
CuB1z <cub1zdev@gmail.com>
2024-06-07 17:03:29 +0200

    chore: Add Google AdSense script for ads on the layout page

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,10 @@ export const metadata = metadataFromConstants
 export default function RootLayout({ children }) {
 	return (
 		<html lang="es">
+			<head>
+				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2182924213276098"
+					crossOrigin="anonymous"></script>
+			</head>
 			<body className={styles.body}>{children}</body>
 		</html>
 	)
