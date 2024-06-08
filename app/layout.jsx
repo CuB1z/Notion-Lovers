2ea801341a6b58f8e9b1effb616b2d22fd272e14
CuB1z <cub1zdev@gmail.com>
2024-06-08 11:47:09 +0200

    refactor: Remove Google AdSense script from layout page and remove ads.txt from public folder

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,10 +6,6 @@ export const metadata = metadataFromConstants
 export default function RootLayout({ children }) {
 	return (
 		<html lang="es">
-			<head>
-				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2182924213276098"
-					crossOrigin="anonymous"></script>
-			</head>
 			<body className={styles.body}>{children}</body>
 		</html>
 	)
