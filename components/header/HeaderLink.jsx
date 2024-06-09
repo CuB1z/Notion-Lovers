26292c5348421e9f485e5e5c3dcec5fed0180236
CuB1z <cub1zdev@gmail.com>
2024-06-09 18:51:33 +0200

    refactor: Update HeaderLink active style logic and add exception for tree page

--- components/header/HeaderLink.jsx
+++ components/header/HeaderLink.jsx
@@ -3,16 +3,23 @@ import styles from "@styles/components/HeaderLink.module.css"
 import { usePathname } from "next/navigation"
 import { useContext } from "react"
 import { MobileContext } from "@/providers/MobileProvider"
+import { AVAILABLE_PAGES } from "@/utils/constants"
 
 export default function HeaderLink({ href, children }) {
     const isMobile = useContext(MobileContext)
 
-    // Add /tree
-
+    // Retrieve the current path and the first part of the href
     const path = usePathname().split("/")[1]
     const fHref = href.split("/")[1]
-    const activeStyle = path === fHref ? styles.active : ""
-    
+
+    // If the current path equals to the first part of the href, set the active style
+    let activeStyle = path === fHref ? styles.active : ""
+
+    // Exceptions for the tree page
+    if (path === AVAILABLE_PAGES.tree.url.split("/")[1] && fHref === AVAILABLE_PAGES.notes.url.split("/")[1]) {
+        activeStyle = styles.active
+    }
+
     return (
         <a className={`${styles.a} ${activeStyle} ${isMobile ? styles.mobile : ""}`} href={href}>
             {children}
