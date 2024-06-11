42c674eb43941a58f30f818a5562e43149046ef7
CuB1z <cub1zdev@gmail.com>
2024-06-11 20:46:54 +0200

    style: Update SideBarItem to use light and dark icons for better visibility

--- components/sidebar/SideBarItem.jsx
+++ components/sidebar/SideBarItem.jsx
@@ -3,7 +3,8 @@ import styles from "@styles/components/SideBarItem.module.css"
 export default function SideBarItem({ item }) {
     return (
         <a className={styles.item} href={item.url}>
-            <img className={styles.img} src="/assets/list-item.svg" alt="arrow" />
+            <img className={`${styles.img} dark-icon`} src="/assets/list-item-light.svg" alt="List Item" />
+            <img className={`${styles.img} light-icon`} src="/assets/list-item.svg" alt="List Item" />
             <span className={styles.title}>{item.title}</span>
         </a>
     )
