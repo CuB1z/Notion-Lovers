0caeceb545a5f2affcb71c103cd080069d1e24f2
CuB1z <cub1zdev@gmail.com>
2024-07-01 15:22:40 +0200

    refactor: Improve Equation component to handle different types of math expressions

--- components/notion/components/Equation.jsx
+++ components/notion/components/Equation.jsx
@@ -1,6 +1,10 @@
 import { InlineMath, BlockMath } from 'react-katex'
 import 'katex/dist/katex.min.css' // Import KaTeX CSS
 
-export default function Equation({ math, inline }) {
-    return inline ? <InlineMath math={math} /> : <BlockMath math={math} />
+export default function Equation(props) {
+    // Retrieve the math expression from the props based on the type of the block
+    const math = props.inline ? props.math : props.block?.properties?.title[0][0]
+
+    // Render the math expression as an inline or block element
+    return props.inline ? <InlineMath math={math} /> : <BlockMath math={math} />
 }
